0e517ed2767adc824ff0499c7e3060c1f8c4c54d
Joshua Myers <myersj281@gmx.com>
2020-02-20 09:02:12 -0500

    Moved main() back to boot to inititalize Omega and pass device defenitions into the kernel.

--- src/Omega.cr
+++ src/Omega.cr
@@ -1,9 +1,24 @@
 require "LibO"
+
 module Omega
-  VERSION       =   "0.1.0"
+  def init(STDOUT : Struct, STDIN : Struct)
+    VERSION       =   "0.1.0"
+    #Initialize drivers
+    
+    def self.print(line : String)
+      STDOUT.print(line)
+    end
+
+    def self.puts(line : String)
+      print line
+      print "\n"
+    end
+
+    def self.clear()
+      STDOUT.clear
+    end
+
+    puts "Welcome to Omega!"
+  end
 end
 
-def main
-  #Initialize core system
-  #puts "Welcome to Omega!"
-end
